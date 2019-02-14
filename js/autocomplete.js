825b245974fcb67d3367f6bab7f2b7ca2181cfb9
nicopowa <nicopowa@gmail.com>
2019-02-14 17:07:45 +0100

    first

---
+++ js/autocomplete.js
@@ -0,0 +1,184 @@
+/**
+* simple autocomplete
+* https://nicopr.fr/autocomplete
+* based on https://www.w3schools.com/howto/howto_js_autocomplete.asp
+*/
+
+/**
+* @nocollapse
+* @export
+*/
+class AutoComplete {
+	
+	constructor(el, options) {
+		this.opt = {};
+		this.options = options;
+		console.log(this.opt);
+		this.dat = [];
+		this.data = this.opt["data"];
+		this.el = el;
+		this.val = "";
+		this.ct = -1;
+		this.down = false;
+		
+		this.empty(this.el);
+		
+		this.inp = document.createElement("input");
+		this.inp.setAttribute("class", "ac-input");
+		this.inp.setAttribute("placeHolder", this.opt["placeHolder"]);
+		this.el.appendChild(this.inp);
+		
+		[["input", this.onInput], ["click", this.onClick], ["keydown", this.onKeyDown], ["blur", this.onBlur]].map(value => this.inp.addEventListener(value[0], value[1].bind(this)));
+		
+		this.cpl = document.createElement("div");
+		this.cpl.style.maxHeight = this.opt["maxHeight"] + "px";
+		this.cpl.setAttribute("class", "ac-items");
+		this.el.appendChild(this.cpl);
+		this.cpl.addEventListener("mousedown", this.onMouseDown.bind(this));
+	}
+	
+	onInput(e) {
+		this.val = this.inp.value.toLowerCase();
+		this.close();
+		if(this.val.length < this.opt["minChars"] || (this.opt["open"] === false && this.cpl.childElementCount)) return;
+		this.filter().map(this.addResult, this);
+	}
+	
+	addResult(result) {
+		let b = document.createElement("div");
+		if(this.opt["highlight"]) {
+			let found = result[this.opt["labelField"]].toLowerCase().indexOf(this.val);
+			b.innerHTML = result[this.opt["labelField"]].slice(0, found) + "<font color=\"#FF9400\">" + result[this.opt["labelField"]].slice(found, found + this.val.length) + "</font>" + result[this.opt["labelField"]].slice(found + this.val.length, result[this.opt["labelField"]].length);
+		}
+		else b.innerHTML = result[this.opt["labelField"]];
+		b.setAttribute("data-value", this.dat.findIndex(value => value[this.opt["labelField"]] == result[this.opt["labelField"]]));
+		b.addEventListener("click", this.select.bind(this));
+		this.cpl.appendChild(b);
+	}
+	
+	filter() {
+		return this.dat.filter(value => value[this.opt["searchField"]].toLowerCase().indexOf(this.val) != -1);
+	}
+	
+	select(e) {
+		let selected = this.dat[e.target.getAttribute("data-value")];
+		this.inp.value = selected[this.opt["labelField"]];
+		this.val = this.inp.value.toLowerCase();
+		this.opt["onSelect"](selected[this.opt["dataField"]]);
+		this.down = false;
+		this.close();
+	}
+	
+	onClick(e) {
+		this.close();
+		if(this.opt["open"] === true && this.val == "") this.dat.map(this.addResult, this);
+		else this.onInput(null);
+	}
+	
+	onKeyDown(e) {
+		let x = this.cpl.childNodes;
+		if([40, 38, 13, 27].indexOf(e.keyCode) != -1) e.preventDefault();
+		if(e.keyCode == 40) { // down
+			if(this.ct == -1) this.onInput(null);
+			this.ct++;
+			this.active(x);
+		}
+		else if(e.keyCode == 38) { // up
+			this.ct--;
+			this.active(x);
+		}
+		else if(e.keyCode == 13) { // enter
+			if(this.ct != -1) x[this.ct].click();
+			else this.close();
+		}
+		else if(e.keyCode == 27) this.close(); // esc
+	}
+	
+	onBlur(e) {
+		if(this.down) return;
+		this.close();
+	}
+	
+	onMouseDown(event) {
+		this.down = true;
+	}
+	
+	active(x) {
+		if(!x.length) return;
+		this.inactive(x);
+		this.ct = this.ct < 0 ? x.length - 1 : (this.ct > x.length - 1 ? 0 : this.ct);
+		x[this.ct].classList.add("ac-active");
+		x[this.ct].scrollIntoView();
+	}
+	
+	inactive(x) {
+		for(let i = 0, l = x.length; i < l; i++) x[i].classList.remove("ac-active");
+	}
+	
+	close() {
+		this.ct = -1;
+		this.empty(this.cpl);
+	}
+	
+	empty(el) {
+		while(el.firstChild) el.removeChild(el.firstChild);
+	}
+	
+	/**
+	* @export
+	*/
+	set value(value) {
+		this.inp.value = value;
+		this.val = value.toLowerCase();
+	}
+	
+	/**
+	* @export
+	*/
+	get value() {
+		let index = this.dat.findIndex(value => value[this.opt["labelField"]].toLowerCase() == this.val);
+		if(index != -1) return this.dat[index][this.opt["dataField"]];
+		return null;
+	}
+	
+	/**
+	* @export
+	*/
+	get input() {
+		return this.inp;
+	}
+	
+	/**
+	* @export
+	*/
+	get box() {
+		return this.cpl;
+	}
+	
+	/**
+	* @export
+	*/
+	set data(value) {
+		if(this.opt["searchField"] == "") {
+			this.dat = value.map(val => ({"text": val}));
+			this.opt["labelField"] = this.opt["searchField"] = this.opt["dataField"] = "text";
+		}
+		else this.dat = value;
+		if(this.opt["searchField"] != this.opt["labelField"]) this.opt["highlight"] = false;
+	}
+	
+	/**
+	* @export
+	*/
+	get data() {
+		return this.dat;
+	}
+	
+	/**
+	* @export
+	*/
+	set options(value) {
+		this.opt = Object.assign.apply(this, [{"placeHolder": "", "open": false, "highlight": true, "maxHeight": 500, "minChars": 3, "labelField": "", "searchField": "", "dataField": "", "data": [], "onSelect": function() {}}, value]);
+		//console.log(this.opt);
+	}
+}
---
+++ js/main.js
@@ -0,0 +1,57 @@
+window.addEventListener("load", main);
+
+function main(event) {
+
+	var fruitsAC = document.createElement("div");
+	fruitsAC.style.width = "500px";
+	fruitsAC.style.height = "39px";
+	document.body.appendChild(fruitsAC);
+
+	var countriesAC = document.createElement("div");
+	countriesAC.style.width = "500px";
+	countriesAC.style.height = "39px";
+	document.body.appendChild(countriesAC);
+
+	var fruits_data = [
+		{"_id": "5c06b5ca35647b127479f560","name": "apple"},
+		{"_id": "5c06b7a8d9482612c30c64a9","name": "banana"},
+		{"_id": "5c06b824d9482612c30c64aa","name": "orange"},
+		{"_id": "5c06b8ecd9482612c30c64ab","name": "pear"},
+		{"_id": "5c08f617e179811518603bad","name": "cherry"},
+		{"_id": "5c08f666e179811518603bae","name": "strawberry"},
+		{"_id": "5c08f6b4e179811518603baf","name": "mango"},
+		{"_id": "5c092ef2e179811518603bb6","name": "tomato"}
+	];
+
+	var countries_data = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Botswana", "Brazil", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "China - Hong Kong / Macau", "Colombia", "Comoros", "Congo", "Congo, Democratic Republic of (DRC)", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "French Guiana", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Great Britain", "Greece", "Grenada", "Guadeloupe", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Israel and the Occupied Territories", "Italy", "Ivory Coast (Cote d'Ivoire)", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Korea, Democratic Republic of (North Korea)", "Korea, Republic of (South Korea)", "Kosovo", "Kuwait", "Kyrgyz Republic (Kyrgyzstan)", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia, Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Moldova, Republic of", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar/Burma", "Namibia", "Nepal", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pacific Islands", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovak Republic (Slovakia)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Netherlands", "Timor Leste", "Togo", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks & Caicos Islands", "Uganda", "Ukraine", "United Arab Emirates", "United States of America (USA)", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (UK)", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
+
+	var fruits = new AutoComplete(fruitsAC, {
+		placeHolder: "Fruits",
+		open: true,
+		highlight: true,
+		data: fruits_data,
+		labelField: "name",
+		searchField: "name",
+		dataField: "_id",
+		minChars: 1,
+		onSelect: function(value) {
+			console.log("select", value);
+			console.log("fruit:", fruits.value);
+		}
+	});
+	
+	var countries = new AutoComplete(countriesAC, {
+		placeHolder: "Countries",
+		open: false,
+		highlight: true,
+		data: countries_data,
+		labelField: "",
+		dataField: "",
+		minChars: 3,
+		onSelect: function(value) {
+			console.log("select", value);
+			console.log("country:", countries.value);
+		}
+	});
+	
+}
